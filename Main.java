526a036cda2c6e2b83ea60fc10399ccc84ff8b77
kanazawa <e1q19024@oit.ac.jp>
2023-01-19 18:42:23 +0900

    mainの変更

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello SEVillans");
+    System.out.println("SEVillains!");
+    Villain villain = new Villain();
+    System.out.println(villain.toString());
     }
 }
